44fb59e316873f339e865ec61454c726f86dc272
Bruna Mello <brunacrespomello@gmail.com>
2023-03-25 12:53:46 +0000

    05. Make the app responsive horizontally
    In this class, we learn:
    
    How to know the current orientation of the device with MediaQuery.of(context).orientation;
    How to make an orientation comparison with a ternary conditional operator;
    Apply a cleaner way to deal with more complex layouts within a ternary conditional operator ( isolating in different widgets ).

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -14,30 +14,71 @@ class Highlights extends StatelessWidget {
       child: CustomScrollView(
         slivers: <Widget>[
           const SliverToBoxAdapter(
-              child: Padding(
-            padding: EdgeInsets.only(bottom: 16.0),
-            child: Text(
-              "Destaques",
-              style: TextStyle(
-                fontFamily: "Caveat",
-                fontSize: 32,
-              ), textAlign: TextAlign.center,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                "Destaques",
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
-          ),
-          SliverList(
-              delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              return HighlightItem(
-                imageURI: items[index]["image"],
-                itemTitle: items[index]["name"],
-                itemPrice: items[index]["price"],
-                itemDescription: items[index]["description"],
-              );
-            },
-            childCount: items.length,
-          ))
+          MediaQuery.of(context).orientation == Orientation.landscape
+              ? const _landscapeList()
+              : const _portraitList(),
         ],
+      ),
+    );
+  }
+}
+
+class _portraitList extends StatelessWidget {
+  const _portraitList({Key? key}) : super(key: key);
+  final List items = destaques;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (context, index) {
+        return HighlightItem(
+          imageURI: items[index]["image"],
+          itemTitle: items[index]["name"],
+          itemPrice: items[index]["price"],
+          itemDescription: items[index]["description"],
+        );
+      },
+      childCount: items.length,
+    ));
+  }
+}
+
+class _landscapeList extends StatelessWidget {
+  const _landscapeList({Key? key}) : super(key: key);
+  final List items = destaques;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1.3,
       ),
     );
   }
